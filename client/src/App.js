fe5e7b4c18c3bf39a2311995c708652a01452872
Ryan Conry <r.conry@hotmail.com>
2019-10-01 15:34:10 -0400

    re-upload of last commit, possible unsaved files

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux"; //provides application with the store
 import store from "./store";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser } from "./actions/authActions";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -22,6 +22,16 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   //set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  //Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    //Logout user
+    store.dispatch(logoutUser());
+    //TODO:clear current profile
+    //Redirect to login
+    window.location.hrf = "/login";
+  }
 }
 
 class App extends Component {
